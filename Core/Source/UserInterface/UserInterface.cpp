28e76749dad4664076c005f0980a912635076a79
Anakorn Songsatain (Muangthai) <nongmuangthai02@gmail.com>
2026-03-20 21:55:22 +0700

    Rewrite 'Library.cmake'

--- Core/Source/UserInterface/UserInterface.cpp
+++ Core/Source/UserInterface/UserInterface.cpp
@@ -3,7 +3,7 @@
 Core::UserInterface::UserInterface(GLFWwindow* window)
 	: window(window)
 {
-	/*IMGUI_CHECKVERSION();
+	IMGUI_CHECKVERSION();
 	ImGui::CreateContext();
 
 	ImGuiIO& io = ImGui::GetIO(); (void)io;
@@ -13,34 +13,34 @@ Core::UserInterface::UserInterface(GLFWwindow* window)
 	io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;
 
 	ImGui_ImplGlfw_InitForOpenGL(window, true);
-	ImGui_ImplOpenGL3_Init("#version 460");*/
+	ImGui_ImplOpenGL3_Init("#version 460");
 }
 
 Core::UserInterface::~UserInterface()
 {
-	/*ImGui_ImplOpenGL3_Shutdown();
+	ImGui_ImplOpenGL3_Shutdown();
 	ImGui_ImplGlfw_Shutdown();
-	ImGui::DestroyContext();*/
+	ImGui::DestroyContext();
 }
 
 void Core::UserInterface::Render()
 {
-	/*ImGui::StyleColorsDark();
+	ImGui::StyleColorsDark();
 
 	float mainScale = ImGui_ImplGlfw_GetContentScaleForMonitor(glfwGetPrimaryMonitor());
 	ImGuiStyle& style = ImGui::GetStyle();
 	style.ScaleAllSizes(mainScale);
-	style.FontScaleDpi = mainScale;*/
+	style.FontScaleDpi = mainScale;
 }
 
 void Core::UserInterface::Update()
 {
-	/*ImGui_ImplOpenGL3_NewFrame();
+	ImGui_ImplOpenGL3_NewFrame();
 	ImGui_ImplGlfw_NewFrame();
 	ImGui::NewFrame();
 
 	ImGui::ShowDemoWindow();
 
 	ImGui::Render();
-	ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());*/
+	ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 }
--- Library.cmake
+++ Library.cmake
@@ -1,49 +1,81 @@
 include(FetchContent)
 
-find_package(glfw3 QUIET)
+find_package(glf3 QUIET)
 if (NOT glfw3_FOUND)
+	set(GLFW_BUILD_DOCS OFF CACHE BOOL "" FORCE)
+	set(GLFW_BUILD_TESTS OFF CACHE BOOL "" FORCE)
+	set(GLFW_BUILD_EXAMPLES OFF CACHE BOOL "" FORCE)
+
 	FetchContent_Declare(
 		glfw3
 		DOWNLOAD_EXTRACT_TIMESTAMP OFF
 		URL https://github.com/glfw/glfw/releases/download/3.4/glfw-3.4.zip
 	)
 
-	FetchContent_GetProperties(glfw3)
-
-	if (NOT glfw3_POPULATED)
-		set(FETCHCONTENT_QUIET FALSE)
-
-		FetchContent_Populate(glfw3)
-
-		add_subdirectory(${glfw3_SOURCE_DIR} ${glfw3_BINARY_DIR})
-	endif()
+	FetchContent_MakeAvailable(glfw3)
 endif()
 
 find_package(OpenGL REQUIRED)
 find_package(Vulkan REQUIRED)
 
-include_directories("Library/glad/include")
-include_directories("Library/stb")
+include_directories(
+	"Library/glad/include"
+	"Library/nlohmann/include"
+)
+
+FetchContent_Declare(
+	stb
+	GIT_REPOSITORY https://github.com/nothings/stb.git
+	GIT_TAG master
+)
+
+FetchContent_MakeAvailable(stb)
+
+include_directories("${stb_SOURCE_DIR}")
 
 find_package(glm QUIET)
 if (NOT glm_FOUND)
 	FetchContent_Declare(
 		glm
 		DOWNLOAD_EXTRACT_TIMESTAMP OFF
-		URL https://github.com/g-truc/glm/archive/refs/tags/1.0.1.zip
+		URL https://github.com/g-truc/glm/releases/download/1.0.3/glm-1.0.3.zip
 	)
 
-	FetchContent_GetProperties(glm)
-
-	if (NOT glm_POPULATED)
-		set(FETCHCONTENT_QUIET FALSE)
-
-		FetchContent_Populate(glm)
-
-		add_subdirectory(${glm_SOURCE_DIR} ${glm_BINARY_DIR})
-	endif()
+	FetchContent_MakeAvailable(glm)
 endif()
-set_target_properties(glm PROPERTIES FOLDER "Library")
 
-include_directories("Library/imgui" "Library/imgui/backends")
-include_directories("Library/nlohmann/include")
+FetchContent_Declare(
+	imgui
+	GIT_REPOSITORY https://github.com/ocornut/imgui.git
+	GIT_TAG docking
+)
+
+FetchContent_MakeAvailable(imgui)
+
+add_library(imgui STATIC
+	"${imgui_SOURCE_DIR}/imgui.cpp"
+	"${imgui_SOURCE_DIR}/imgui_draw.cpp"
+	"${imgui_SOURCE_DIR}/imgui_tables.cpp"
+	"${imgui_SOURCE_DIR}/imgui_widgets.cpp"
+	"${imgui_SOURCE_DIR}/imgui_demo.cpp"
+	"${imgui_SOURCE_DIR}/backends/imgui_impl_glfw.cpp"
+	"${imgui_SOURCE_DIR}/backends/imgui_impl_opengl3.cpp"
+)
+
+target_include_directories(imgui PUBLIC "${imgui_SOURCE_DIR}" "${imgui_SOURCE_DIR}/backends")
+target_link_libraries(imgui PUBLIC glfw OpenGL::GL Vulkan::Vulkan)
+set_target_properties(imgui PROPERTIES FOLDER "Library")
+
+FetchContent_Declare(
+	imguizmo
+	GIT_REPOSITORY https://github.com/CedricGuillemet/ImGuizmo.git
+	GIT_TAG master
+)
+
+FetchContent_MakeAvailable(imguizmo)
+
+add_library(imguizmo STATIC "${imguizmo_SOURCE_DIR}/ImGuizmo.cpp")
+
+target_include_directories(imguizmo PUBLIC "${imguizmo_SOURCE_DIR}")
+target_link_libraries(imguizmo PUBLIC imgui)
+set_target_properties(imguizmo PROPERTIES FOLDER "Library")
