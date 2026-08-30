4939f46f9a5e01773889ed245b0cf267149886ca
Anakorn Songsatain (Muangthai) <nongmuangthai02@gmail.com>
2026-08-30 14:53:18 +0700

    Update GLFW version

--- Library.cmake
+++ Library.cmake
@@ -9,7 +9,7 @@ if (NOT glfw3_FOUND)
 	FetchContent_Declare(
 		glfw3
 		DOWNLOAD_EXTRACT_TIMESTAMP OFF
-		URL https://github.com/glfw/glfw/releases/download/3.4/glfw-3.4.zip
+		URL https://github.com/glfw/glfw/releases/download/3.5.1/glfw-3.5.1.zip
 	)
 
 	FetchContent_MakeAvailable(glfw3)
